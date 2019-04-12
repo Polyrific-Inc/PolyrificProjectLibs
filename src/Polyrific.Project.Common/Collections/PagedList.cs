e89366dabdd5c2843274caf453764a785c81a410
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2019-04-12 13:04:08 +0700

    Init Polyrific.Project.Common

---
+++ src/Polyrific.Project.Common/Collections/PagedList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Polyrific.Project.Common.Collections
+{
+    public class PagedList<T> : List<T>
+    {
+        /// <summary>
+        /// Instantiate <see cref="PagedList"/> object
+        /// </summary>
+        /// <param name="items">Collection of the items</param>
+        /// <param name="pageIndex">Current page index</param>
+        /// <param name="pageSize">Maximum number of items in a page</param>
+        /// <param name="totalCount">Total number of all items</param>
+        public PagedList(List<T> items, int pageIndex, int pageSize, int totalCount)
+        {
+            AddRange(items);
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            LastPageIndex = (int)(totalCount / pageSize);
+        }
+
+        /// <summary>
+        /// Current page index
+        /// </summary>
+        /// <value></value>
+        public int PageIndex { get; } = 0;
+
+        /// <summary>
+        /// Maximum number of items in a page
+        /// </summary>
+        /// <value></value>
+        public int PageSize { get; } = 10;
+
+        /// <summary>
+        /// Last page index
+        /// </summary>
+        /// <value></value>
+        public int LastPageIndex { get; } = 0;
+    }
+}
---
+++ src/Polyrific.Project.Common/Helpers/FileHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Polyrific.Project.Common.Helpers
+{
+    /// <summary>
+    /// Helper to support unavailable implementations of <see cref="System.IO.File"/> in netstandard2.0
+    /// </summary>
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Read the file content asynchronously.
+        /// </summary>
+        /// <param name="path">Path to the file</param>
+        /// <returns></returns>
+        public static async Task<string> ReadAllTextAsync(string path)
+        {
+            string text;
+            using (var reader = File.OpenText(path))
+            {
+                text = await reader.ReadToEndAsync();
+            }
+
+            return text;
+        }
+
+        /// <summary>
+        /// Write file content asynchronously. If the file exists, its content will be replaced.
+        /// </summary>
+        /// <param name="path">Path to the file</param>
+        /// <param name="content">The string to write as file content</param>
+        /// <returns></returns>
+        public static async Task WriteAllTextAsync(string path, string content)
+        {
+            using (var writer = File.CreateText(path))
+            {
+                await writer.WriteAsync(content);
+
+                await writer.FlushAsync();
+            }
+        }
+
+        /// <summary>
+        /// Write file content asynchronously. If the file exists, it will append the content.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static async Task AppendAllTextAsync(string path, string content)
+        {
+            using (var writer = File.AppendText(path))
+            {
+                await writer.WriteAsync(content);
+
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
